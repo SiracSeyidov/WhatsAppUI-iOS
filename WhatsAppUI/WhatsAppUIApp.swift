f3ee1bfb44af5f9c36b51c67611bb8bd2240fc53
SeyidovSirac <predatorman4@gmail.com>
2024-05-20 08:32:05 +0400

    Initial Commit

---
+++ WhatsAppUI/WhatsAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppUIApp.swift
+//  WhatsAppUI
+//
+//  Created by Sirac Seyidov on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
